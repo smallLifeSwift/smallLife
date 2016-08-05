fb95aadba1bb6ad61a0972361d1098a0e1471b7e
Stago <99765996@qq.com>
2016-08-05 11:46:53 +0800

    no message

---
+++ SmallDay-new/SmallDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmallDay
+//
+//  Created by perhapsdone@163.com on 16/8/5.
+//  Copyright © 2016年 perhapsdone@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
