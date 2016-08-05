b49bb58ea8bdef62bc5f3825d710e931e00886d0
Stago <99765996@qq.com>
2016-08-05 13:39:02 +0800

    no message

---
+++ SmallDay-new/SmallDay/Classes/SmallDay-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SmallDay-Bridging-Header.h
+//  SmallDay
+//
+//  Created by perhapsdone@163.com on 16/8/5.
+//  Copyright © 2016年 perhapsdone@163.com. All rights reserved.
+//
+
+#ifndef SmallDay_Bridging_Header_h
+#define SmallDay_Bridging_Header_h
+
+
+#endif /* SmallDay_Bridging_Header_h */
